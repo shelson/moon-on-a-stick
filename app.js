661b7e5dd9ac289475f2cd8d23cb04e65acc7394
Simon Helson <simon.helson@gmail.com>
2023-03-09 00:04:26 +0000

    This better work :)

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,10 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist/client')));
 
-// app.use('/', indexRouter);
-//app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
+app.use('/', express.static(path.join(__dirname, 'dist/client'));
+app.use('*', express.static(path.join(__dirname, 'dist/client'));
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -37,5 +29,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use('/', express.static(path.join(__dirname, 'dist/client'));
+app.use('*', express.static(path.join(__dirname, 'dist/client'));
 
 module.exports = app;
